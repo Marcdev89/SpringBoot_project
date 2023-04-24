35cff3bfef60e63bacb378e9efb9129e69a2f202
Marcdev89 <marc.vitoria89@gmail.com>
2023-04-24 14:26:01 +0200

    added .map to print users table from controller

--- src/main/resources/static/js/users.js
+++ src/main/resources/static/js/users.js
@@ -1,4 +1,31 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+getUsers();
+  $('#usersTable').DataTable();
 });
+
+async function getUsers(){
+
+    const request = await fetch('users',{
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    });
+    const users = await request.json();
+
+    let usersList = '';
+    users.map((e) =>{
+       usersList += '<tr> <td>#'+ e.id +'1</td><td>'
+                        + e.name + ' '
+                        + e.surname +'</td><td>'
+                        + e.mail + '</td><td>'
+                        + e.phone
+                        + '</td> <td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td></tr>>';
+
+    })
+
+document.querySelector('#usersTable tbody').outerHTML = usersList;
+
+}
