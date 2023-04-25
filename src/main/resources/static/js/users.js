11cf4488fbfe5af40d315023d2f9461982da6c83
Marcdev89 <marc.vitoria89@gmail.com>
2023-04-25 14:07:59 +0200

    New path and added delete function

--- src/main/resources/static/js/users.js
+++ src/main/resources/static/js/users.js
@@ -6,7 +6,7 @@ getUsers();
 
 async function getUsers(){
 
-    const request = await fetch('users',{
+    const request = await fetch('api/users',{
         method: 'GET',
         headers: {
             'Accept': 'application/json',
@@ -17,15 +17,33 @@ async function getUsers(){
 
     let usersList = '';
     users.map((e) =>{
+     let btnDelete = '<a href="#" onclick="deleteUser('+ e.id +')"class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a>'
+
        usersList += '<tr> <td>#'+ e.id +'1</td><td>'
                         + e.name + ' '
                         + e.surname +'</td><td>'
-                        + e.mail + '</td><td>'
+                        + e.email + '</td><td>'
                         + e.phone
-                        + '</td> <td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td></tr>>';
+                        + '</td> <td>' + btnDelete + '</td></tr>>';
 
     })
 
 document.querySelector('#usersTable tbody').outerHTML = usersList;
 
+}
+
+async function deleteUser(id){
+
+    if(!confirm('Really want to delete this user?')){
+        return;
+    }
+
+    const request = await fetch('api/users/' + id,{
+        method: 'DELETE',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    });
+    location.reload();
 }
