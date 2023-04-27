b3ba90c48aaca723546536730423ac1eed583356
Marcdev89 <marc.vitoria89@gmail.com>
2023-04-27 11:27:39 +0200

    Implemented verify function

--- src/main/java/com/javacourse/course/controllers/UserController.java
+++ src/main/java/com/javacourse/course/controllers/UserController.java
@@ -1,14 +1,10 @@
 package com.javacourse.course.controllers;
-
 import com.javacourse.course.models.User;
 import com.javacourse.course.repository.IUser;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
@@ -16,28 +12,19 @@ public class UserController {
     @Autowired
     private IUser iUser;
 
-    @RequestMapping(value = "api/user")
-    public User getUser1(){
-        User user = new User();
-        user.setId(1L);
-        user.setName("Lucas");
-        user.setSurname("Marx");
-        user.setEmail("lucasmarx@gmail.com");
-        user.setPhone("666999888");
-        return user;
+
+    @RequestMapping(value = "api/users", method = RequestMethod.POST)
+    public void createNewUser(@RequestBody User user){
+
+        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+        String hash = argon2.hash(1,1024,1, user.getPassword());
+        user.setPassword(hash);
+
+
+        iUser.register(user);
     }
 
-    @RequestMapping(value = "api/users/{id}")
-    public User getUser(@PathVariable Long id){
-        User user = new User();
-        user.setName("Lucas");
-        user.setSurname("Marx");
-        user.setEmail("lucasmarx@gmail.com");
-        user.setPhone("666999888");
-        return user;
-    }
-
-    @RequestMapping(value = "api/users")
+    @RequestMapping(value = "api/users", method = RequestMethod.GET)
     public List<User> getUsers(){
         return iUser.getUsers();
     }
@@ -45,6 +32,8 @@ public class UserController {
     public void delete(@PathVariable Long id) {
         iUser.delete(id);
     }
+
+
 
 
 }
--- src/main/java/com/javacourse/course/repository/UserInterfaceImp.java
+++ src/main/java/com/javacourse/course/repository/UserInterfaceImp.java
@@ -1,6 +1,8 @@
 package com.javacourse.course.repository;
-
 import com.javacourse.course.models.User;
+
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
@@ -11,9 +13,10 @@ import java.util.List;
 @Transactional
 public class UserInterfaceImp implements IUser{
     @PersistenceContext
-    private EntityManager entityManager;
+    EntityManager entityManager;
 
     @Override
+    @Transactional
     public List<User> getUsers() {
         String query = "FROM User";
         return entityManager.createQuery(query).getResultList();
@@ -23,5 +26,28 @@ public class UserInterfaceImp implements IUser{
     public void delete(Long id) {
         User user = entityManager.find(User.class, id);
         entityManager.remove(user);
+    }
+
+    @Override
+    public void register(User user) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    public boolean verifyEmailPassword(User user){
+        String query = "FROM User WHERE email = :email";
+        List<User> list = entityManager.createQuery(query)
+            .setParameter("email", user.getEmail())
+                    .getResultList();
+
+        if (list.isEmpty()){
+            return false;
+        }
+
+        String passwordHashed = list.get(0).getPassword();
+
+        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+        return argon2.verify(passwordHashed, user.getPassword());
+
     }
 }
